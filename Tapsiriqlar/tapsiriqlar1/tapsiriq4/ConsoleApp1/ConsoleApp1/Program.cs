026de3911a2810a1ab8018ec1faf7daa9fc67da6
sananrr <rhlisna19@gmail.com>
2023-08-29 15:30:27 +0400

    Doruduncu tapsiriq elave olundu

---
+++ Tapsiriqlar/tapsiriqlar1/tapsiriq4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0-dan 100-ə qədər olan ədədləri çapa verən proqram tərtib edin.
+            //Console.WriteLine("0-dan 100-e qeder olan ededler:");
+            //for (int i = 0; i < 100; i++) 
+            //{
+
+            //    Console.WriteLine(i);
+            //}
+
+            //0 - dan 100 - ə qədər olan cüt ədədləri çap edən proqram tərtib edin.
+
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //Console.WriteLine("0-dan 100-e qeder olan cut ededler:");
+
+            //Console.WriteLine("0-dan 100-e qeder olan t ededler:");
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            // 0-dan 100-ə qədər olan ədədlər içərisində rəqəmləri bir birinə bərabər olan iki rəqəmli ədədləri çap edən proqram tərtib edin.
+
+            //Console.WriteLine("0-dan 100-e qeder tekliyi ve onlugu beraber olan ededler: ");
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    int onluq = i / 10;
+            //    int teklik = i % 10;
+
+            //    if (onluq == teklik)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    }
+
+
+
+                Console.ReadLine();
+        }
+        
+    }
+}
