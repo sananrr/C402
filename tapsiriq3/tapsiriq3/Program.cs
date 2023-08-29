8c43a3c26c99d77b550ab025720c59861b7acebe
sananrr <rhlisna19@gmail.com>
2023-08-29 14:40:24 +0400

    ucuncu tapsiriq elave olundu

---
+++ tapsiriq3/tapsiriq3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tapsiriq3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  {"salam","masin","buz","ev","kod","meymun","avtobus"} arrayinda 4 hərfdən az olan sözləri çapa verən proqram tərtib edin.
+
+            string[] words = { "salam", "masin", "buz", "ev", "kod", "meymun", "avtobus" };
+
+            Console.WriteLine("4 herfden az olan sozler: ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+
+                if (i < 4)
+                {
+                    Console.WriteLine(words[i]);
+                }
+
+                
+
+                // { "salam","masin","buz","ev","kod","meymun","avtobus"} arrayinda 4 hərfdən çox olan sözlərin sonuna AZE ifadəsini əlavə edərək çapa verən proqram tərtib edin.
+
+                else
+                {
+                    Console.WriteLine(words[i] + "AZE");
+                }
+            }
+
+            Console.WriteLine("#########");
+            //{ "salam","masin","buz","ev","kod","meymun","avtobus"} arrayinda olan elementləri çapa verən proqram tərtib edin.
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            //{ "salam","masin","buz","ev","kod","meymun","avtobus"} arrayinin elemetlərini arasında boşluqlar olmaq şərti ilə yan-yana düzən proqram tərtib edin.
+            Console.WriteLine("#########");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i]);
+                Console.Write(" ");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
