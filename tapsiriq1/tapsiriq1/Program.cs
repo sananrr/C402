feb64e946eec6689d79d21529049e9b1d6ea79a1
sananrr <rhlisna19@gmail.com>
2023-08-29 12:53:41 +0400

    Birinci tapsiriq elave olundu

---
+++ tapsiriq1/tapsiriq1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tapsiriq1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            // İki ədədin cəmini tapan funksiya yazın. Funksiya dəyişənləri parametr olaraq almalıdır.
+            int toplama(int a, int b)
+            {
+                return a + b;
+            }
+            
+             void ededler()
+            {
+                Console.Write("Enter the first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the second number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int result = toplama(num1, num2);
+                Console.WriteLine("The sum is: " + result);
+            }
+            ededler();
+            Console.ReadLine();
+        }
+    }
+}
